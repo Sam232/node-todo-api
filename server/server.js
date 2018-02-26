512cd3cbd970b672f2d9292defd3f8bda956c4d2
Samuel <jread912@gmail.com>
2018-02-25 21:37:22 -0800

    Added Code To Handle PATCH HTTP REQUEST To This Endpoint And Update A Todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const {ObjectID} = require("mongodb");
+const _ = require("lodash");
 
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/Todo");
@@ -84,6 +85,47 @@ app.delete("/todos/:id", (req, res) => {
         statusCode: 400
       });
     });
+  }
+
+  res.status(400).send({
+    message: "Invalid Todo ID Provided",
+    statusCode: 400
+  });
+});
+
+app.patch("/todos/:id/", (req, res) => {
+  var todoID = req.params.id;
+  var body = _.pick(req.body, ["text", "completedAt"]);
+
+  if(ObjectID.isValid(todoID)){
+      if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+      }
+      else{
+        body.completedAt = null;
+        body.completed = false
+      }
+
+      return Todo.findByIdAndUpdate(todoID, {
+        $set: body
+      }, {new: true}).then((doc) => {
+        if(doc){
+          return res.send({
+            doc,
+            statusCode: 200
+          });
+        }
+        res.status(400).send({
+          message: "No Todo\'s ID Matches The Provided ID",
+          statusCode: 400
+        });
+      })
+      .catch((e) => {
+        res.status(400).send({
+          message: "Unable To Process Request",
+          statusCode: 400
+        });
+      });
   }
 
   res.status(400).send({
